e3ef786babc15d4b31349edde64fac7887d6c5ea
Hao Chen <mmkrty@gmail.com>
2022-09-23 15:49:55 +0200

    add class View, complete search view

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,18 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-const recipeContainer = document.querySelector('.recipe');
 
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
@@ -28,8 +33,28 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+
+    //1) get query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    //2) load results
+    await model.loadSearchResults(query);
+
+    //3)render results
+    resultsView.render(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -4,11 +4,15 @@ import { getJSON } from './helpers';
 
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: [],
+  },
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}/${id}`);
+    const data = await getJSON(`${API_URL}${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -25,5 +29,27 @@ export const loadRecipe = async function (id) {
   } catch (error) {
     console.error(`${error} 💔`);
     throw error;
+  }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    console.log(data);
+
+    state.search.results = data.data.recipes.map(recipe => {
+      return {
+        id: recipe.id,
+        title: recipe.title,
+        publisher: recipe.publisher,
+        image: recipe.image_url,
+      };
+    });
+
+    console.log(state.search.results);
+  } catch (err) {
+    throw err;
   }
 };
